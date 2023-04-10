28d0aeddeffc582e1fda21057ee85e45a0d19aaa
navekedemdooble <nave@dooble.co.il>
2023-04-10 20:15:24 +0300

    Create Login page

--- client/ibkr-bot/src/pages/LoginPage.tsx
+++ client/ibkr-bot/src/pages/LoginPage.tsx
@@ -1,11 +1,19 @@
-import { Box, Center, Flex,Text  } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import loginBG from '../assets/login-bg.jpg'
+import IBKR from '../assets/IBKR.png'
 
 export const LoginPage: React.FC = () => {
     return <Flex color='white' height={'100vh'}>
         <Center flex='1'>
-            <Text>Box 1</Text>
+            <Box display={"flex"} textAlign={'center'} color={"#000"} flexDirection={"column"} padding={"0 5rem"}>
+                <img src={IBKR} style={{ height: 200, width: 200, objectFit: "contain", margin: 'auto' }}></img>
+                <Heading as='h1' margin={"1rem 0"} size='xl'>Welcome to Interactive Brokers Trading Bot</Heading>
+                <Text fontSize={"1.3rem"}>Before you are login into the system, Download the TWS desktop app and login to your interactive brokers account</Text>
+                <Button colorScheme='red' margin={"3rem auto"} fontSize={"1.3rem"} width={"30%"} onClick={() =>{}}>
+                    Login
+                </Button>
+            </Box>
         </Center>
         <Box flex='1' bg='tomato' backgroundImage={loginBG}>
         </Box>
